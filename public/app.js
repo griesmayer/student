c842a5f6b846afef332eb9beb5cd17874e28b7bc
Thomas Griesmayer <griesmayer@spengergasse.at>
2025-10-30 23:19:51 +0100

    Add Student implemented

--- public/app.js
+++ public/app.js
@@ -40,5 +40,44 @@ async function fetchStudents() {
   }
 }
 
-// When page isloaded the fetchStudents is called
+async function addStudent(name, course) {
+  const statusEl = document.getElementById("status");
+  try {
+    const res = await fetch("/students", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, course }),
+    });
+    if (!res.ok) throw new Error(`HTTP ${res.status}`);
+    await fetchStudents();
+    statusEl.textContent = "Student added.";
+  } catch (err) {
+    console.error(err);
+    statusEl.textContent = `Error while added a student: ${err.message}`;
+  }
+}
+
+async function addClick() {
+    const nameInput   = document.getElementById("student-name");
+    const name = nameInput.value.trim();
+    const courseInput = document.getElementById("student-course");
+    const course = courseInput.value.trim();
+    const button = document.getElementById("add-btn");
+    const statusEl = document.getElementById("status");
+
+    if (!name || !course) {
+      statusEl.textContent = "Name and course required.";
+      return;
+    }
+
+    button.disabled = true;
+    await addStudent(name, course);
+    button.disabled = false;
+
+    nameInput.value = "";
+    courseInput.value = "";
+    nameInput.focus();
+}
+
+// When page is loaded the fetchStudents is called
 window.addEventListener("DOMContentLoaded", fetchStudents);
