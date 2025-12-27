e0615a8f677c3c83fc6f0a7de9f75992daec45d1
Georg Graf (SPG) <grafg@spengergasse.at>
2025-12-27 14:44:40 +0100

    auch server arbeitet mit prisma jetzt

---
+++ server.ts
@@ -0,0 +1,68 @@
+import express from "express";
+import { fromFileUrl } from "@std/path";
+import { PrismaClient } from "./prisma/generatedclient/client.ts";
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+const port = Deno.env.get("PORT") || 3000;
+// **NEW** add the path module
+// **NEW** use the public/index.htlm file
+
+const publicDir = fromFileUrl(new URL("./public", import.meta.url));
+app.use(express.static(publicDir));
+
+app.get("/", (req, res) => {
+  res.send("Hello, World from Express!");
+});
+
+app.get("/students", async (req, res) => {
+  const students = await prisma.student.findMany();
+  res.json(students);
+});
+
+app.get("/students/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const student = await prisma.student.findUnique({
+    where: { id },
+  });
+  if (!student) {
+    return res.status(404).json({ error: "Student not found" });
+  }
+  res.json(student);
+});
+app.post("/students", async (req, res) => {
+  const { name, course } = req.body;
+  if (!name || !course) {
+    return res.status(400).json({ error: "Name and course are required!" });
+  }
+  const newStudent = await prisma.student.create({
+    data: { name, course },
+  });
+  res.status(201).json(newStudent);
+});
+
+app.patch("/students/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name, course } = req.body;
+
+  if (!name || !course) {
+    return res.status(400).json({ error: "Name and course are required!" });
+  }
+  const patchedStudent = prisma.student.update({
+    where: { id },
+    data: { name, course },
+  });
+  res.json(patchedStudent); // TODO fix for wrong id
+});
+
+app.delete("/students/:id", async (req, res) => {
+  await prisma.student.delete({
+    where: { id: parseInt(req.params.id) },
+  });
+  return res.status(204).send(); // No Content
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
