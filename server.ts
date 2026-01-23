c317e2bfd60dc49de919e95ad30240a1db8dbca0
Thomas Griesmayer <griesmayer@spengergasse.at>
2026-01-23 14:35:15 +0100

    umstellung

---
+++ server.ts
@@ -0,0 +1,93 @@
+import express, { Request, Response } from "express";
+import path from "node:path";
+import process from "node:process";
+import { PrismaClient } from "./generated/client.ts";
+
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
+// **NEW** use the public/index.htlm file
+app.use(express.static(path.join(import.meta.dirname || __dirname, "public")));
+
+const prisma = new PrismaClient();
+
+const students = await prisma.student.findMany();
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello, World from Express!");
+});
+
+app.get("/students", (_req: Request, res: Response) => {
+  res.json(students);
+});
+
+app.get("/students/:id", (req: Request, res: Response) => {
+  const id = parseInt(String(req.params.id));
+  const student = students.find(s => s.id === id);
+
+  if (!student) {
+    return res.status(404).json({ error: "Student not found" });
+  }
+
+  res.json(student);
+});
+
+app.post("/students", (req: Request, res: Response) => {
+  const { name, course } = req.body;
+
+  if (!name || !course) {
+    return res.status(400).json({ error: "Name and course are required!" });
+  }
+
+  const newId = students.length ? students[students.length - 1].id + 1 : 1;
+  const newStudent = { id: newId, name, course };
+  students.push(newStudent);
+  res.status(201).json(newStudent);
+});
+
+app.put("/students/:id", (req: Request, res: Response) => {
+  const id = parseInt(String(req.params.id));
+  const { name, course } = req.body;
+
+  if (!name || !course) {
+    return res.status(400).json({ error: "Name and course are required!" });
+  }
+
+  const pos = students.findIndex(s => s.id === id);
+  if (pos === -1) {
+    return res.status(404).json({ error: "Student not found" });
+  }
+  students[pos] = { id, name, course };
+  res.json(students[pos]);
+});
+
+app.patch("/students/:id", (req: Request, res: Response) => {
+  const id = parseInt(String(req.params.id));
+  const { name, course } = req.body;
+
+  const student = students.find(s => s.id === id);
+  if (!student) {
+    return res.status(404).json({ error: "Student not found" });
+  }
+
+  if (name !== undefined) student.name = name;
+  if (course !== undefined) student.course = course;
+
+  res.json(student);
+});
+
+app.delete("/students/:id", (req: Request, res: Response) => {
+  const id = parseInt(String(req.params.id));
+  const pos = students.findIndex(s => s.id === id);
+
+  if (pos === -1) {
+    return res.status(404).json({ error: "Student not found" });
+  }
+
+  students.splice(pos, 1);
+  return res.status(204).send(); // No Content
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
